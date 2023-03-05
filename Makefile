984c862b02b69c4f96ea728273db4e53512e1ead
alexaib <alexaib@outlook.es>
2023-03-05 23:26:39 +0100

    Updates on main test

--- Makefile
+++ Makefile
@@ -1,23 +1,32 @@
-NAME=gnl.out
+BIN=gnl.out
 
 CC=gcc
 CFLAGS=-Wall -Werror -Wextra
+DEBUG_FLAGS=-fsanitize=address -g
 
-MAIN_C=main.c
-SRCS=get_next_line.c get_next_line_utils.c $(MAIN_C)
+SRCS=get_next_line.c get_next_line_utils.c main.c
+OBJS=$(SRCS:.c=.o)
 
-.PHONY: all run clean $(NAME)
+BUFFER_SIZE=42
+
+.PHONY: all run clean fclean $(NAME)
 
 all: $(NAME)
 
-run: $(NAME)
-	@./$(NAME)
-
-$(NAME):
-	@$(CC) -g $(SRCS) -o $(NAME)
-
-$(NAME)_strict:
-	@$(CC) $(CFLAGS) $(SRCS) -o $(NAME)_strict
+run: $(BIN)
+	@./$(BIN)
 
 clean:
-	@rm -rf $(NAME)
+	@rm -f $(OBJS)
+
+fclean: clean
+	@echo "Cleaning all"
+	@rm -f $(BIN)
+
+re: fclean all
+
+$(BIN): $(OBJS)
+	@$(CC) $(DEBUG_FLAGS) -o $(BIN) $(OBJS)
+
+%.o: %.c
+	@$(CC) $(CFLAGS) $(DEBUG_FLAGS) -o $@ -c $<
--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: alexaib <alexaib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 12:21:00 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/03/04 22:01:27 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/03/05 15:40:38 by alexaib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,35 @@
 #include <stdio.h>
 #include <stdio.h>
 
-int main() {
-	int fildes = open("test.txt", O_RDONLY);
-	void *buf;
-	size_t nbytes;
-	char *line;
-	int i;
+static void file1_gnl() {
+	int		fildes;
+	char	*line;
+	int		i;
 
 	// first, try reading from file
+	fildes = open("test.txt", O_RDONLY);
 	line = get_next_line(fildes);
+	i = 0;
 	while (line) {
 		printf("Linea %i: %s", i++, line);
+		free(line);
 		line = get_next_line(fildes);
 	}
+	free(line);
+}
 
+static void stdio_gnl() {
 	// try reading from stdio
-	line = get_next_line(0);
+	char *line = get_next_line(0);
 	printf("stdin: %s", line);
+	free(line);
+}
+
+int main() {
+	file1_gnl();
+	stdio_gnl();
+	// TODO implement checks for
+	//  empty file
+	//  invalid fd
 	return 0;
 }
